e3b1e558fbecfc02f4c70018b1f5c56f91f31240
Laurent Zhang <laurentz@arcology.network>
2024-02-17 00:39:47 -0700

    Add test case for ParaCumU256Sub

--- tests/multiprocess_test.go
+++ tests/multiprocess_test.go
@@ -345,3 +345,13 @@ func TestSimpleConflict(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func TestParaCumU256Sub(t *testing.T) {
+	currentPath, _ := os.Getwd()
+	targetPath := path.Join(path.Dir(filepath.Dir(currentPath)), "concurrentlib/lib/")
+
+	err, _, _ := DeployThenInvoke(targetPath, "multiprocess/multiprocess_test.sol", "0.8.19", "ParaCumU256SubTest", "call()", []byte{}, false)
+	if err != nil {
+		t.Error(err)
+	}
+}
