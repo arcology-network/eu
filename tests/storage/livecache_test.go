d5ca4a5c453d77c4062c34c437eb2ab7bef9e5c4
Laurent Zhang <laurentz@arcology.network>
2024-09-11 00:23:28 -0600

    refactor: Add benchmark test for LiveCache. Removed UpdateStats() and fixed a couple of bugs in profile.go

--- tests/storage/livecache_test.go
+++ tests/storage/livecache_test.go
@@ -20,9 +20,14 @@ package stgtest
 import (
 	"fmt"
 	"testing"
+	"time"
 
+	"github.com/arcology-network/eu/eth"
+	statestore "github.com/arcology-network/storage-committer"
+	stgcommcommon "github.com/arcology-network/storage-committer/common"
 	stgcommon "github.com/arcology-network/storage-committer/common"
 	cache "github.com/arcology-network/storage-committer/storage/livecache"
+	stgproxy "github.com/arcology-network/storage-committer/storage/proxy"
 	commutative "github.com/arcology-network/storage-committer/type/commutative"
 	noncommutative "github.com/arcology-network/storage-committer/type/noncommutative"
 	univalue "github.com/arcology-network/storage-committer/type/univalue"
@@ -52,13 +57,49 @@ func TestLiveCache(t *testing.T) {
 
 	// outV, _, _ := univalue.NewUnivalue(1, "blcc://eth1.0/account/"+alice+"/storage/native/"+RandomKey(0), new(noncommutative.Bytes))
 	liveCache := cache.NewLiveCache(151)
-	liveCache.Commit([]*univalue.Univalue{metaIn, u256In, bytesIn})
+	liveCache.Commit([]*univalue.Univalue{metaIn, u256In, bytesIn}, 1)
 	liveCache.Print()
 
 	bytes = noncommutative.NewBytes([]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
 	bytesIn = univalue.NewUnivalue(1, "blcc://eth1.0/account/"+alice+"/storage/bytes", 3, 4, 5, bytes, nil)
 	fmt.Println("bytesIn size: ", bytesIn.Value().(stgcommon.Type).MemSize())
 
-	liveCache.Commit([]*univalue.Univalue{bytesIn})
+	liveCache.Commit([]*univalue.Univalue{bytesIn}, 2)
 	liveCache.Print()
+}
+
+func BenchmarkLiveCache(b *testing.B) {
+	store := chooseDataStore()
+
+	sstore := statestore.NewStateStore(store.(*stgproxy.StorageProxy))
+	writeCache := sstore.WriteCache
+	alice := AliceAccount()
+	if _, err := eth.CreateNewAccount(stgcommcommon.SYSTEM, alice, writeCache); err != nil { // NewAccount account structure {
+		fmt.Println(err)
+	}
+
+	path := commutative.NewPath() // create a path
+	if _, err := writeCache.Write(0, "blcc://eth1.0/account/"+alice+"/storage/ctrn-0/", path); err != nil {
+		b.Error(err)
+	}
+
+	for i := 0; i < 1000; i++ {
+		if _, err := writeCache.Write(0, "blcc://eth1.0/account/"+alice+"/storage/ctrn-0/elem-0"+fmt.Sprint(i), noncommutative.NewString(string("test"))); err != nil { /* The first Element */
+			b.Error(err)
+			break
+		}
+	}
+
+	trans := writeCache.Export()
+
+	liveCache := cache.NewLiveCache(1280)
+	t0 := time.Now()
+	liveCache.Commit(trans, 1)
+	fmt.Println("liveCache Commit time: ", liveCache.Length(), liveCache.Size(), time.Since(t0))
+
+	// if liveCache.Length() != 11 || liveCache.Size() != 194 {
+	// 	b.Error("Length is not 11 or the size is not 194")
+	// }
+
+	// fmt.Println("Count, Size in Bytes:", liveCache.Length(), liveCache.Size())
 }
